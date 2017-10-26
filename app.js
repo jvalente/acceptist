942f2b3d46e947fd45c804cb2267298875c90889
João Valente <filipevalente@gmail.com>
2017-10-26 19:09:08 +0900

    properly parses body response

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.get('/twist/incoming', function (req, res) {
-    console.log(req)
-    console.log(res)
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/twist/incoming', function (req, res) {
+    console.log(req.body)
     res.send('Hello World!')
 })
 
