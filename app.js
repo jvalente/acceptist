5ca67b4a255cb3e9da22782eef5b9b23932c50ba
João Valente <filipevalente@gmail.com>
2017-10-26 19:59:05 +0900

    adds TD task on /twist/incoming event

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const Todoist = require('./todoist')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/twist/incoming', function (req, res) {
-    console.log(req.body)
-    res.send('Hello World!')
+    const content = req.body
+
+    if (content.thread_title) {
+        Todoist.addTask(content.thread_title)
+                .then((body) => {
+                    console.log(body)
+                    res.send(`Added ${content.thread_title} to Todoist.`)
+                }).catch(function (err) {
+                    console.log(err)
+                    res.send('Could not add to Todoist')
+                });
+    } else {
+        res.send('Sorry. Acceptist only works on threads for now.')
+    }
 })
 
 app.listen(3000, function () {
